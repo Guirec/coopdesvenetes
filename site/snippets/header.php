3a9a85a56e5de613fccd97926a366d359645e115
Jerome Tan <jerome.tan@tuta.io>
2024-03-20 16:01:03 +0100

    added logo in header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -50,33 +50,27 @@
   <link rel="shortcut icon" type="image/x-icon" href="<?= url('favicon.ico') ?>">
 </head>
 <body>
-
   <header class="header">
-    <?php
-    /*
-      We use `$site->url()` to create a link back to the homepage
-      for the logo and `$site->title()` as a temporary logo. You
-      probably want to replace this with an SVG.
-    */
-    ?>
     <a class="logo" href="<?= $site->url() ?>">
-      <?= $site->title()->esc() ?>
+      <?php if ($logo = $site->file($site->logo())): ?>
+        <?= $logo ?>
+      <?php endif ?>
     </a>
-
     <nav class="menu">
       <a <?php e($site->homePage()->isOpen(), 'aria-current="page"') ?> href="<?= $site->homePage()->url() ?>">Accueil</a>
+
       <?php
-      /*
-        In the menu, we only fetch listed pages,
-        i.e. the pages that have a prepended number
-        in their foldername.
+        /*
+          In the menu, we only fetch listed pages,
+          i.e. the pages that have a prepended number
+          in their foldername.
 
-        We do not want to display links to unlisted
-        `error`, `home`, or `sandbox` pages.
+          We do not want to display links to unlisted
+          `error`, `home`, or `sandbox` pages.
 
-        More about page status:
-        https://getkirby.com/docs/reference/panel/blueprints/page#statuses
-      */
+          More about page status:
+          https://getkirby.com/docs/reference/panel/blueprints/page#statuses
+        */
       ?>
       <?php foreach ($site->children()->listed() as $item): ?>
       <a <?php e($item->isOpen(), 'aria-current="page"') ?> href="<?= $item->url() ?>"><?= $item->title()->esc() ?></a>
