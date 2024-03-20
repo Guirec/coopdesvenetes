61d67bc0a5a75bce8ec56598a7685b3f47a4949c
Jerome Tan <jerome.tan@tuta.io>
2024-03-20 16:01:03 +0100

    added logo in header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -52,36 +52,35 @@
 <body>
 
   <header class="header">
-    <?php
-    /*
-      We use `$site->url()` to create a link back to the homepage
-      for the logo and `$site->title()` as a temporary logo. You
-      probably want to replace this with an SVG.
-    */
-    ?>
-    <a class="logo" href="<?= $site->url() ?>">
-      <?= $site->title()->esc() ?>
-    </a>
-
-    <nav class="menu">
       <?php
       /*
-        In the menu, we only fetch listed pages,
-        i.e. the pages that have a prepended number
-        in their foldername.
-
-        We do not want to display links to unlisted
-        `error`, `home`, or `sandbox` pages.
-
-        More about page status:
-        https://getkirby.com/docs/reference/panel/blueprints/page#statuses
+        We use `$site->url()` to create a link back to the homepage
+        for the logo and `$site->title()` as a temporary logo. You
+        probably want to replace this with an SVG.
       */
       ?>
-      <?php foreach ($site->children()->listed() as $item): ?>
-      <a <?php e($item->isOpen(), 'aria-current="page"') ?> href="<?= $item->url() ?>"><?= $item->title()->esc() ?></a>
-      <?php endforeach ?>
-      <?php snippet('social') ?>
-    </nav>
+      <a class="logo" href="<?= $site->url() ?>">
+        <img src="assets/images/logo_coop_des_venetes.jpg" alt="Logo Coop des Vénètes">
+      </a>
+      <nav class="menu">
+        <?php
+        /*
+          In the menu, we only fetch listed pages,
+          i.e. the pages that have a prepended number
+          in their foldername.
+
+          We do not want to display links to unlisted
+          `error`, `home`, or `sandbox` pages.
+
+          More about page status:
+          https://getkirby.com/docs/reference/panel/blueprints/page#statuses
+        */
+        ?>
+        <?php foreach ($site->children()->listed() as $item): ?>
+        <a <?php e($item->isOpen(), 'aria-current="page"') ?> href="<?= $item->url() ?>"><?= $item->title()->esc() ?></a>
+        <?php endforeach ?>
+        <?php snippet('social') ?>
+      </nav>
   </header>
 
   <main class="main">
