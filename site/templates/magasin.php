e6fbce5a599c2758d2aea799fbd2d47d27054df6
Jerome Tan <jerome.tan@tuta.io>
2024-03-29 16:41:32 +0100

    added a layout on magasin page (content, blueprint and template)

--- site/templates/magasin.php
+++ site/templates/magasin.php
@@ -20,11 +20,18 @@
 ?>
 <?php snippet('header') ?>
 
-<article>
-  <h1 class="h1"><?= $page->title()->esc() ?></h1>
-  <div class="text">
-    <?= $page->text()->kt() ?>
+<h1 class="h1"><?= $page->title()->esc() ?></h1>
+
+<?php foreach ($page->layout()->toLayouts() as $layout): ?>
+<section class="grid" id="<?= $layout->id() ?>">
+  <?php foreach ($layout->columns() as $column): ?>
+  <div class="column" style="--span:<?= $column->span() ?>">
+    <div class="blocks">
+      <?= $column->blocks() ?>
+    </div>
   </div>
-</article>
+  <?php endforeach ?>
+</section>
+<?php endforeach ?>
 
 <?php snippet('footer') ?>
