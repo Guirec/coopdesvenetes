cdb25719d5aeb50a0280cb8ca11829079b3d989d
Jerome Tan <jerome.tan@tuta.io>
2024-08-28 16:58:15 +0200

    created intro custom block

--- site/snippets/blocks/intro.php
+++ site/snippets/blocks/intro.php
@@ -1,21 +1,19 @@
 <div class="introduction">
-  <div class="introduction-heading">
-    <h2>
-      <?= $block->heading() ?>
-    </h2>
+  <div class="introduction-heading heading-1">
+    <?= $block->heading() ?>
   </div>
-  <div class="introduction-info">
-    <div class="introduction-details introduction-address">
-      <?php if($icon = $block->address_icon()->toFile()): ?>
+  <ul role="list" class="introduction-info">
+    <li class="introduction-details">
+      <?php if ($icon = $block->address_icon()->toFile()): ?>
         <img src="<?= $icon->url() ?>" alt="">
-      <?php endif ?>
+      <?php endif; ?>
       <p><?= $block->address() ?></p>
-    </div>
-    <div class="introduction-details introduction-opening-times">
-      <?php if($icon = $block->opening_times_icon()->toFile()): ?>
+    </li>
+    <li class="introduction-details">
+      <?php if ($icon = $block->opening_times_icon()->toFile()): ?>
           <img src="<?= $icon->url() ?>" alt="">
-      <?php endif ?>
+      <?php endif; ?>
       <p><?= $block->opening_times() ?></p>
-    </div>
-  </div>
+    </li>
+  </ul>
 </div>
