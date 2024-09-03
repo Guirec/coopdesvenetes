4c77e54ff727d95354f3207b79452dfb0fd02217
Guirec Lefort <bonjour@guirec.dev>
2024-09-03 13:23:14 +0200

    Update store block link markup
    Semantic markup and prevent too wide clickable zone

--- site/snippets/blocks/store.php
+++ site/snippets/blocks/store.php
@@ -9,16 +9,16 @@
     <div class="store-text">
       <?= $block->store_description()->kirbytext() ?>
     </div>
-    <a class="push-down" href="<?= $block->link() ?>">
-      <div class="button">
-        <p><?= $block->button_text() ?> </p>
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" fill="currentColor"/>
-        </svg>
-      </div>
-    </a>
   </div>
 		<div class="store-image">
 			<img src="<?= $image->url() ?>" alt="<?= $image->alt() ?>">
+		</div>
+		<div class="push-down">
+			<a class="button" href="<?= $block->link() ?>">
+				<?= $block->button_text() ?>
+				<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+					<path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"/>
+				</svg>
+			</a>
 		</div>
 </div>
