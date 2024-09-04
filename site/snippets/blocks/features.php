89917a72376d074f237507c34d52486dd769a91a
Jerome Tan <jerome.tan@tuta.io>
2024-09-05 00:45:15 +0200

    implemented feature custom block

--- site/snippets/blocks/features.php
+++ site/snippets/blocks/features.php
@@ -1,2 +1,8 @@
 <div class="features">
+  <?php foreach ($block->icons()->toFiles() as $icon): ?>
+    <div class="feature <?= $icon->shape() ?>">
+      <?= $icon ?>
+      <p><?= $icon->feature_name() ?></p>
+  </div>
+  <?php endforeach ?>
 </div>
