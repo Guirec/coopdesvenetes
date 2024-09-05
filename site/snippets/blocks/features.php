1e8d90b46a316fd55b5e3513bb292e725f3a5813
Jerome Tan <jerome.tan@tuta.io>
2024-09-05 16:12:08 +0200

    added svg icons

--- site/snippets/blocks/features.php
+++ site/snippets/blocks/features.php
@@ -1,8 +1,10 @@
-<div class="features">
-  <?php foreach ($block->icons()->toFiles() as $icon): ?>
-    <div class="feature <?= $icon->shape() ?>">
-      <?= $icon ?>
-      <p><?= $icon->feature_name() ?></p>
-  </div>
-  <?php endforeach ?>
-</div>
+<section class="features">
+  <ul class="features__list">
+    <?php foreach ($block->icons()->toFiles() as $icon): ?>
+      <li class="features__item">
+        <img src="<?= $icon->url() ?>" width="<?= $icon->width() ?>" height="<?= $icon->height() ?>">
+        <p><?= $icon->feature_name() ?></p>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</section>
