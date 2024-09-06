65e0b26d2019ae41dd3bb8451ed6801fc8127d25
Jerome Tan <jerome.tan@tuta.io>
2024-09-06 11:08:31 +0200

    refactored custom blocks

--- site/snippets/blocks/intro.php
+++ site/snippets/blocks/intro.php
@@ -1,15 +1,15 @@
 <div class="introduction">
-  <div class="introduction-heading heading-1">
+  <div class="introduction__heading heading-1">
     <?= $block->heading() ?>
   </div>
-  <ul role="list" class="introduction-info">
-    <li class="introduction-details">
+  <ul role="list" class="introduction__info">
+    <li class="introduction__details">
       <?php if ($icon = $block->address_icon()->toFile()): ?>
         <img src="<?= $icon->url() ?>" alt="">
       <?php endif; ?>
       <?= $block->address()->kirbytext() ?>
     </li>
-    <li class="introduction-details">
+    <li class="introduction__details">
       <?php if ($icon = $block->opening_times_icon()->toFile()): ?>
           <img src="<?= $icon->url() ?>" alt="">
       <?php endif; ?>
